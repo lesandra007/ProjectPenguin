3d289345a2c6c1c33cb20f6bf66dd3c2798f7a7b
AnneMai <annepmaimain@gmail.com>
2024-03-03 12:31:43 -0800

    trying

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar';
+import Title from '../components/Title';
 
 export default function Home() {
-  return (
-    <div className="PageMenuAndContent">
-        <Sidebar/>
-        <h1>Home</h1>
-    </div>
-  );
+    return (
+        <div style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'row', backgroundColor: '#324376'}}>
+            <Sidebar/>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
+                <Title title='PROJECT PENGUIN'/>
+            </div>
+        </div>
+    );
 }
