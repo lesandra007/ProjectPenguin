2123a996fe4a4e843fc4be0dd9d5014354b4e777
jsmn.lao <jsmn.lao@gmail.com>
2024-03-10 11:23:11 -0700

    updated questions page

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -7,10 +7,13 @@ export default function Questions() {
   return (
     <div className="PageMenuAndContent">
         <Sidebar/>
-        {/* <h1>Questions</h1> */}
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
-                <Title title='Questions'/>
-            </div>
+          <Title title='Questions'/>
+        </div>
+        <div style={{position: 'absolute', left: '300px', top: '150px', background: "black", height: '200px', width: '300px', padding: '50px', color: '#FFFFFF'}}>
+          <h2>Question of the Day</h2>
+          <p>Test Message</p>
+        </div>
     </div>
   );
 }
