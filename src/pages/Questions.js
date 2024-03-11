03d489a35488cc265772730faf5a506d7ac2605d
jsmn.lao <jsmn.lao@gmail.com>
2024-03-10 20:14:59 -0700

    updated questions page

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar';
+import "../App.css";
 import Title from '../components/Title';
 
 
@@ -10,10 +11,23 @@ export default function Questions() {
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
           <Title title='Questions'/>
         </div>
-        <div style={{position: 'absolute', left: '300px', top: '150px', background: "black", height: '200px', width: '300px', padding: '50px', color: '#FFFFFF'}}>
-          <h2>Question of the Day</h2>
-          <p>Test Message</p>
-        </div>
+        {/* <div style={{position: 'absolute', left: '300px', top: '150px', background: "black", height: '200px', width: '300px', padding: '50px', color: '#FFFFFF'}}> */}
+        {/* <div className="QuestionItems">
+          <div className="question">
+            <h2>Question of the Day</h2>
+            <p>Test Message</p>
+          </div>
+        </div> */}
+         <div className="QuestionItems">
+              <div className="question">
+                <h2>Question of the Day</h2>
+                <p>Describe a time when you had to work with a difficult team member. What actions did you take to resolve the situation to encourage the team's ongoing progress?</p>
+              </div>
+              <div className="question">
+                <h2>Your Response</h2>
+                <p>Type your response here...</p>
+              </div>
+          </div>
     </div>
   );
 }
