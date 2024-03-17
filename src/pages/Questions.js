6c5c3128214eae78e3508e50aeb628e327786596
jsmn.lao <jsmn.lao@gmail.com>
2024-03-16 20:05:59 -0700

    updated question page with text area and submit button

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -5,19 +5,20 @@ import Title from '../components/Title';
 
 
 export default function Questions() {
+  const handleSubmit = () => {
+    const submit = window.confirm('Confirm to submit response!');
+    if (submit) {
+        console.log('Form submitted!');
+    }
+  };
+
+  
   return (
     <div className="PageMenuAndContent">
         <Sidebar/>
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
           <Title title='Questions'/>
         </div>
-        {/* <div style={{position: 'absolute', left: '300px', top: '150px', background: "black", height: '200px', width: '300px', padding: '50px', color: '#FFFFFF'}}> */}
-        {/* <div className="QuestionItems">
-          <div className="question">
-            <h2>Question of the Day</h2>
-            <p>Test Message</p>
-          </div>
-        </div> */}
          <div className="QuestionItems">
               <div className="question">
                 <h2>Question of the Day</h2>
@@ -26,6 +27,10 @@ export default function Questions() {
               <div className="question">
                 <h2>Your Response</h2>
                 <p>Type your response here...</p>
+                <form onSubmit={handleSubmit}>
+                  <textarea type="text" style={{ width: '300px', height: '100px', textAlign: 'justify', resize: 'none'}}/>
+                  <button type="submit" className="submitButton">Submit </button>
+                </form>
               </div>
           </div>
     </div>
