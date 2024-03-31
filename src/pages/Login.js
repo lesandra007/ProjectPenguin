e2260be8effede6647ddfe475dc22beac4924a26
Sandra Le <lesandra007@gmail.com>
2024-03-30 19:49:13 -0700

    formatting

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,31 +15,33 @@ export default function Login() {
 
     return (
         <div className={'outerContainer'}>
-            <div className={'logoContainer'}>
-                <img src={penguinlogo} alt="logo" className="logo"/>
-            </div>
-            <div className={'titleContainer'}>
-                <div>Project Penguin</div>
-            </div>
-            <div classname={'inputContainer'} >
-                <input 
-                    value={email}
-                    placeholder={"Email"}
-                    onChange={(input) => setEmail(input.target.value)}
-                    type="text"
-                />
-            </div>
-            <div classname={'inputContainer'} >
-                <input
-                    value={password}
-                    placeholder={"Password"}
-                    onChange={(input) => setPassword(input.target.value)}
-                    type="text"
-                    classname="inputBox"
-                />
-            </div>
-            <div className={'buttonContainer'}>
-                <input className={"button"} type="button" onClick={onButtonClick} value={"Log In"} />
+            <div className='loginBox'>
+                <div className={'logoContainer'}>
+                    <img src={penguinlogo} alt="logo" className="logo"/>
+                </div>
+                <div className={'titleContainer'}>
+                    <div>Project Penguin</div>
+                </div>
+                <div classname={'inputContainer'} >
+                    <input 
+                        value={email}
+                        placeholder={"Email"}
+                        onChange={(input) => setEmail(input.target.value)}
+                        type="text"
+                    />
+                </div>
+                <div classname={'inputContainer'} >
+                    <input
+                        value={password}
+                        placeholder={"Password"}
+                        onChange={(input) => setPassword(input.target.value)}
+                        type="text"
+                        classname="inputBox"
+                    />
+                </div>
+                <div className={'buttonContainer'}>
+                    <input className={"button"} type="button" onClick={onButtonClick} value={"Log In"} />
+                </div>
             </div>
         </div>
     );
