49c4422beb2cbcb4faa735b42424e46eb4a6d386
jsmn.lao <jsmn.lao@gmail.com>
2024-04-14 11:44:12 -0700

    add responsiveness to questions page

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -16,23 +16,28 @@ export default function Questions() {
   return (
     <div className="PageMenuAndContent">
         <Sidebar/>
+        <div className="PageContent">
         <div style={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
           <Title title='Questions'/>
         </div>
          <div className="QuestionItems">
               <div className="question">
                 <h2>Question of the Day</h2>
-                <p>Describe a time when you had to work with a difficult team member. What actions did you take to resolve the situation to encourage the team's ongoing progress?</p>
+                <p>Describe a time when you had to work with a 
+                  difficult team member. What actions did you take
+                  to resolve the situation to encourage the team's 
+                  ongoing progress?</p>
               </div>
               <div className="question">
                 <h2>Your Response</h2>
                 <p>Type your response here...</p>
                 <form onSubmit={handleSubmit}>
-                  <textarea type="text" style={{ width: '300px', height: '100px', textAlign: 'justify', resize: 'none'}}/>
+                  <textarea type="text"/>
                   <button type="submit" className="submitButton">Submit </button>
                 </form>
               </div>
           </div>
+        </div>
     </div>
   );
 }
